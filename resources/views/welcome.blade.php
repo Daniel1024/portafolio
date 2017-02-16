cbce177ebe9a8676b369523b45a93f5272f44146
Daniel López <tierra_santa_9@hotmail.com>
2017-02-16 04:03:10 +0000

    Probando el webhook

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Daniel López
                 </div>
 
                 <div class="links">
